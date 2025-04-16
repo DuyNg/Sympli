9087c19e6add77ac8bc7a1e5a7a4cc97992789f3
Duy Nguyen <DNguyen.ND@gmail.com>
2025-04-17 02:59:00 +0700

    config header request for axois

---
+++ fe/sympli-search-spa/src/api/axiosInstance.ts
@@ -0,0 +1,16 @@
+// src/api/axiosInstance.ts
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7250/api/';
+
+export const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Max-Age": "3600",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  },
+});
--- fe/sympli-search-spa/src/api/searchApi.ts
+++ fe/sympli-search-spa/src/api/searchApi.ts
@@ -1,17 +1,10 @@
 // src/api/searchApi.ts
-import axios from 'axios';
+import { apiClient } from './axiosInstance';
 import { SearchRequest, SearchResult } from '../models';
 
-const API_URL = 'https://localhost:7250/api/search';
-
 export const searchKeyword = async (payload: SearchRequest): Promise<SearchResult> => {
-  const url = `${API_URL}/${encodeURIComponent(payload.browser)}/${encodeURIComponent(payload.keyword)}/${encodeURIComponent(payload.url)}`;
-
-  const response = await axios.get<SearchResult>(url, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const url = `search/${encodeURIComponent(payload.browser)}/${encodeURIComponent(payload.keyword)}/${encodeURIComponent(payload.url)}`;
+  const response = await apiClient.get<SearchResult>(url);
   return response.data;
 };
 
